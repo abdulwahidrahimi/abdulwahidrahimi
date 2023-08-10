7161560a20e8f389cfa9eb8ea1fe30f949c40aa7
abdulwahidrahimi <140418649+abdulwahidrahimi@users.noreply.github.com>
2023-08-10 00:09:10 -0400

    Add files via upload

---
+++ day 2/script.js
@@ -0,0 +1,7 @@
+document.querySelector(".navbar-toggle").addEventListener("click", function () {
+    let links = document.querySelectorAll(".navbar-links");
+    for (let i = 0; i < links.length; i++) {
+      links[i].classList.toggle("active");
+    }
+    console.log("toggle")
+});
